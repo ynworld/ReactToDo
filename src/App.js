b73fd34dce9ecfe99a5372e417f9d57ada673974
Yura <ynworld85@mac.com>
2023-02-01 10:42:38 +0300

    feat: add static html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-  return <h1>Todo App</h1>;
+  return (
+    <div className="wrapper">
+      <h1 className="heading-1">Tasks</h1>
+      <ul className="todo__list">
+        <li className="todo__list-item">
+          Dentist Appointment - Prepare the files and do the necessary tests
+          suggested beforehand.
+        </li>
+      </ul>
+      <button className="button-add">+</button>
+    </div>
+  );
 }
 
 export default App;
