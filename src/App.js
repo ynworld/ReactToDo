389724427a50bd967832e0203cd6397d8cd30aea
Yura <ynworld85@mac.com>
2023-02-01 17:20:38 +0300

    fix: alignment, button type

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import { IconContext, Square, CheckSquare } from 'phosphor-react';
 
 function App() {
+  const todoItems = [
+    'Dentist Appointment - Prepare the files and do the necessary tests suggested beforehand.',
+    'CS-121 Assignment Deadline on Monday',
+    'Get the electric bulb repaired',
+    'Soccer Club Meeting @ Sunday',
+    "Buy Gift for Dad's Birthday",
+    'Submit Assignment on Friday',
+    'Bring Groceries From Supermarket',
+  ];
   return (
     <IconContext.Provider
       value={{
@@ -13,47 +22,18 @@ function App() {
       <div className="wrapper">
         <h1 className="heading-1">Tasks</h1>
         <ul className="todo__list">
-          <li className="todo__list-item">
-            <Square className="list-item__icon" />
-            <span className="list-item__text">
-              Dentist Appointment - Prepare the files and do the necessary tests
-              suggested beforehand.
-            </span>
-          </li>
-          <li className="todo__list-item">
-            <Square />
-            <span className="list-item__text">
-              CS-121 Assignment Deadline on Monday
-            </span>
-          </li>
-          <li className="todo__list-item">
-            <Square />
-            <span className="list-item__text">
-              Get the electric bulb repaired
-            </span>
-          </li>
-          <li className="todo__list-item">
-            <CheckSquare weight="fill" />
-            <span className="list-item__text">
-              Soccer Club Meeting @ Sunday
-            </span>
-          </li>
-          <li className="todo__list-item">
-            <CheckSquare weight="fill" />
-            <span className="list-item__text">Buy Gift for Dad's Birthday</span>
-          </li>
-          <li className="todo__list-item">
-            <CheckSquare weight="fill" />
-            <span className="list-item__text">Submit Assignment on Friday</span>
-          </li>
-          <li className="todo__list-item">
-            <CheckSquare weight="fill" />
-            <span className="list-item__text">
-              Bring Groceries From Supermarket
-            </span>
-          </li>
+          {todoItems.map(item => (
+            <li key={item}>
+              <article className="todo__list-item">
+                <Square className="list-item__icon" />
+                <span className="list-item__text">{item}</span>
+              </article>
+            </li>
+          ))}
         </ul>
-        <button className="button-add">+</button>
+        <button type="submit" className="button-add">
+          +
+        </button>
       </div>
     </IconContext.Provider>
   );
