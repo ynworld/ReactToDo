bc9e07d9ac9dc726447382703b41a4f499113cc2
Yura <ynworld85@mac.com>
2023-02-01 11:51:38 +0300

    styles: add layout and color

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import './App.css';
-import { Square, CheckSquare } from 'phosphor-react';
+import { IconContext, Square, CheckSquare } from 'phosphor-react';
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1 className="heading-1">Tasks</h1>
-      <ul className="todo__list">
-        <li className="todo__list-item">
-          <Square size={24} />
-          <span>
-            Dentist Appointment - Prepare the files and do the necessary tests
-            suggested beforehand.
-          </span>
-        </li>
-        <li className="todo__list-item">
-          <Square size={24} />
-          <span>CS-121 Assignment Deadline on Monday</span>
-        </li>
-        <li className="todo__list-item">
-          <Square size={24} />
-          <span>Get the electric bulb repaired</span>
-        </li>
-        <li className="todo__list-item">
-          <CheckSquare size={24} weight="fill" />
-          <span>Soccer Club Meeting @ Sunday</span>
-        </li>
-        <li className="todo__list-item">
-          <CheckSquare size={24} weight="fill" />
-          <span>Buy Gift for Dad's Birthday</span>
-        </li>
-        <li className="todo__list-item">
-          <CheckSquare size={24} weight="fill" />
-          <span>Submit Assignment on Friday</span>
-        </li>
-        <li className="todo__list-item">
-          <CheckSquare size={24} weight="fill" />
-          <span>Bring Groceries From Supermarket</span>
-        </li>
-      </ul>
-      <button className="button-add">+</button>
-    </div>
+    <IconContext.Provider
+      value={{
+        color: '#4fc41d',
+        size: 32,
+        weight: 'bold',
+      }}
+    >
+      <div className="wrapper">
+        <h1 className="heading-1">Tasks</h1>
+        <ul className="todo__list">
+          <li className="todo__list-item">
+            <Square className="list-item__icon" />
+            <span className="list-item__text">
+              Dentist Appointment - Prepare the files and do the necessary tests
+              suggested beforehand.
+            </span>
+          </li>
+          <li className="todo__list-item">
+            <Square />
+            <span className="list-item__text">
+              CS-121 Assignment Deadline on Monday
+            </span>
+          </li>
+          <li className="todo__list-item">
+            <Square />
+            <span className="list-item__text">
+              Get the electric bulb repaired
+            </span>
+          </li>
+          <li className="todo__list-item">
+            <CheckSquare weight="fill" />
+            <span className="list-item__text">
+              Soccer Club Meeting @ Sunday
+            </span>
+          </li>
+          <li className="todo__list-item">
+            <CheckSquare weight="fill" />
+            <span className="list-item__text">Buy Gift for Dad's Birthday</span>
+          </li>
+          <li className="todo__list-item">
+            <CheckSquare weight="fill" />
+            <span className="list-item__text">Submit Assignment on Friday</span>
+          </li>
+          <li className="todo__list-item">
+            <CheckSquare weight="fill" />
+            <span className="list-item__text">
+              Bring Groceries From Supermarket
+            </span>
+          </li>
+        </ul>
+        <button className="button-add">+</button>
+      </div>
+    </IconContext.Provider>
   );
 }
 
