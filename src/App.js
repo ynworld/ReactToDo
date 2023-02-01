60f00a1bce820ff218ba18420f3e1a791f8f0626
Yura <ynworld85@mac.com>
2023-02-01 10:56:16 +0300

    feat: add icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Square, CheckSquare } from 'phosphor-react';
 
 function App() {
   return (
@@ -6,8 +7,35 @@ function App() {
       <h1 className="heading-1">Tasks</h1>
       <ul className="todo__list">
         <li className="todo__list-item">
-          Dentist Appointment - Prepare the files and do the necessary tests
-          suggested beforehand.
+          <Square size={24} />
+          <span>
+            Dentist Appointment - Prepare the files and do the necessary tests
+            suggested beforehand.
+          </span>
+        </li>
+        <li className="todo__list-item">
+          <Square size={24} />
+          <span>CS-121 Assignment Deadline on Monday</span>
+        </li>
+        <li className="todo__list-item">
+          <Square size={24} />
+          <span>Get the electric bulb repaired</span>
+        </li>
+        <li className="todo__list-item">
+          <CheckSquare size={24} weight="fill" />
+          <span>Soccer Club Meeting @ Sunday</span>
+        </li>
+        <li className="todo__list-item">
+          <CheckSquare size={24} weight="fill" />
+          <span>Buy Gift for Dad's Birthday</span>
+        </li>
+        <li className="todo__list-item">
+          <CheckSquare size={24} weight="fill" />
+          <span>Submit Assignment on Friday</span>
+        </li>
+        <li className="todo__list-item">
+          <CheckSquare size={24} weight="fill" />
+          <span>Bring Groceries From Supermarket</span>
         </li>
       </ul>
       <button className="button-add">+</button>
