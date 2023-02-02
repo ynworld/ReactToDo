a46331e10ee5fb81be0e58fd2a3d9633637db055
Yura <ynworld85@mac.com>
2023-02-02 13:14:34 +0300

    feat: add id field to todo items

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ const todos = [
   {
     text: 'Dentist Appointment - Prepare the files and do the necessary tests suggested beforehand.',
     isChecked: false,
+    id: 'td1',
   },
 
   {
     text: 'CS-121 Assignment Deadline on Monday',
     isChecked: false,
+    id: 'td2',
   },
   {
     text: 'Get the electric bulb repaired',
     isChecked: false,
+    id: 'td3',
   },
   {
     text: 'Soccer Club Meeting @ Sunday',
     isChecked: true,
+    id: 'td4',
   },
   {
     text: "Buy Gift for Dad's Birthday",
     isChecked: true,
+    id: 'td5',
   },
   {
     text: 'Submit Assignment on Friday',
     isChecked: true,
+    id: 'td6',
   },
   {
     text: 'Bring Groceries From Supermarket',
     isChecked: true,
+    id: 'td7',
   },
 ]
 
@@ -47,12 +54,12 @@ function App() {
         <h1 className="heading-1">Tasks</h1>
         <ul className="todo__list">
           {todos.map((todo) => (
-            <li key={todo.text}>
+            <li key={todo.id}>
               <article className="todo__list-item">
-                <label htmlFor={todo.text} className="todo__list-label">
+                <label htmlFor={todo.id} className="todo__list-label">
                   {todo.isChecked && <CheckSquare className="list-item__icon" weight="fill" />}
                   {!todo.isChecked && <Square className="list-item__icon" />}
-                  <input type="checkbox" id={todo.text} className="list-item__checkbox" />
+                  <input type="checkbox" id={todo.id} className="list-item__checkbox" />
                   <span className="list-item__text">{todo.text}</span>
                 </label>
               </article>
