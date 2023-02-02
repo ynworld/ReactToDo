2ea684fd7f4ee99d079a82d1ea0638d3553f1026
Yura <ynworld85@mac.com>
2023-02-02 16:03:25 +0300

    refactor: add CSS custom checkbox, add common API to CheckboxField, use valid class names for CheckboxField scope

--- src/components/CheckboxField.js
+++ src/components/CheckboxField.js
@@ -1,13 +1,17 @@
-import CheckboxIcon from '../UI/CheckboxIcon'
 import './CheckboxField.css'
 
-const CheckboxField = ({ todo }) => {
-  const { id, text, isChecked } = todo
+const CheckboxField = ({ label, id, isChecked, onChange }) => {
   return (
-    <label htmlFor={id} className="todo__list-label">
-      <CheckboxIcon isChecked={isChecked} />
-      <input type="checkbox" id={id} className="list-item__checkbox" />
-      <span className="list-item__text">{text}</span>
+    <label htmlFor={id} className="label">
+      <input
+        type="checkbox"
+        id={id}
+        className="checkbox"
+        defaultChecked={isChecked}
+        onChange={onChange}
+      />
+      <span className="checkbox-custom" />
+      <span className="checkbox__label">{label}</span>
     </label>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,10 @@ import './TodoItem.css'
 import CheckboxField from './CheckboxField'
 
 const TodoItem = ({ todo }) => {
+  const { id, text, isChecked } = todo
   return (
     <article className="todo__list-item">
-      <CheckboxField todo={todo} />
+      <CheckboxField id={id} label={text} isChecked={isChecked} onChange={() => {}} />
     </article>
   )
 }
