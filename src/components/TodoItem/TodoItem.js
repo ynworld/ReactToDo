fb75f3429f932c4d5399450dc4a9ddaba30bba55
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-02-04 23:11:22 +0300

    refactor: move todo item logic from TodoList to TodoItem

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,12 +1,59 @@
 import { observer } from 'mobx-react'
 
-import './TodoItem.css'
 import { CheckboxField } from '../CheckboxField'
+import { CheckCircleIcon, CloseIcon, EditIcon, TrashIcon } from '../icons'
 
-const TodoItem = ({ todo, deleteItem, passInputValue }) => {
+import './TodoItem.css'
+
+const TodoItem = ({ todo, canEdit, deleteItem }) => {
+  let inputValue = ''
+
+  const passInputValue = (text) => {
+    inputValue = text
+  }
+
+  const submitEditHandler = (todo) => {
+    let text = ''
+    if (inputValue === '') text = todo.text
+    else text = inputValue
+    if (text.trim().length !== 0) todo.editItem(text)
+    else todo.editItem('New To Do')
+  }
+
+  const cancelEditHandler = (todo) => {
+    if (!todo.id) {
+      deleteItem(todo)
+
+      return
+    }
+
+    todo.setIsEditing(false)
+  }
+
   return (
     <article className="todo__list-item">
       <CheckboxField todo={todo} deleteItem={deleteItem} passInputValue={passInputValue} />
+      <div className="edit__icons">
+        {todo.isEditing ? (
+          <>
+            <span onClick={cancelEditHandler.bind(null, todo)}>
+              <CloseIcon className="edit__icons-icon" />
+            </span>
+            <span onClick={submitEditHandler.bind(null, todo)}>
+              <CheckCircleIcon className="edit__icons-icon" />
+            </span>
+          </>
+        ) : (
+          <>
+            <span onClick={canEdit ? todo.setIsEditing.bind(null, true) : undefined}>
+              <EditIcon className="edit__icons-icon" />
+            </span>
+            <span onClick={deleteItem.bind(null, todo)}>
+              <TrashIcon className="edit__icons-icon" />
+            </span>
+          </>
+        )}
+      </div>
     </article>
   )
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,62 +3,18 @@ import { observer } from 'mobx-react'
 import { TodoItem } from '../TodoItem'
 import './TodoList.css'
 
-import { TrashIcon, EditIcon, CloseIcon, CheckCircleIcon } from '../icons'
-
 const TodoList = ({ todoList }) => {
-  let inputValue = ''
-  const passInputValue = (text) => {
-    inputValue = text
-  }
-
   const { hasItemInEditingMode, items: todos, deleteItem } = todoList
-
-  const canEdit = !hasItemInEditingMode
-
-  const submitEditHandler = (todo) => {
-    let text = ''
-    if (inputValue === '') text = todo.text
-    else text = inputValue
-    if (text.trim().length !== 0) todo.editItem(text)
-    else todo.editItem('New To Do')
-  }
-
-  const cancelEditHandler = (todo) => {
-    if (!todo.id) {
-      deleteItem(todo)
-
-      return
-    }
-
-    todo.setIsEditing(false)
-  }
 
   return (
     <ul className="todo__list">
       {todos.map((todo) => (
         <li key={todo.key} className="list-item">
-          <TodoItem todo={todo} deleteItem={deleteItem} passInputValue={passInputValue} />
-          <div className="edit__icons">
-            {todo.isEditing ? (
-              <>
-                <span onClick={cancelEditHandler.bind(null, todo)}>
-                  <CloseIcon className="edit__icons-icon" />
-                </span>
-                <span onClick={submitEditHandler.bind(null, todo)}>
-                  <CheckCircleIcon className="edit__icons-icon" />
-                </span>
-              </>
-            ) : (
-              <>
-                <span onClick={canEdit ? todo.setIsEditing.bind(null, true) : undefined}>
-                  <EditIcon className="edit__icons-icon" />
-                </span>
-                <span onClick={deleteItem.bind(null, todo)}>
-                  <TrashIcon className="edit__icons-icon" />
-                </span>
-              </>
-            )}
-          </div>
+          <TodoItem
+            todo={todo}
+            deleteItem={deleteItem}
+            canEdit={!hasItemInEditingMode}
+          />
         </li>
       ))}
     </ul>
