9e05359f09aba17ac67c4045f23f9ee865317b3a
Yura <ynworld85@mac.com>
2023-02-06 00:34:06 +0300

    feat: add TrashIcon to TodoItem

--- client/src/components/TodoItem/TodoItem.js
+++ client/src/components/TodoItem/TodoItem.js
@@ -2,13 +2,19 @@ import { observer } from 'mobx-react'
 
 import './TodoItem.css'
 import { CheckboxField } from '../CheckboxField'
+import { TrashIcon } from '../icons'
 
 const TodoItem = ({ todo }) => {
+  const handleItemDelete = () => todo.delete()
+
   const { id, text, isChecked, toggle } = todo
 
   return (
     <article className="todo__list-item">
       <CheckboxField id={id} label={text} isChecked={isChecked} onChange={toggle} />
+      <div className="edit__icons" onClick={handleItemDelete}>
+        <TrashIcon className="edit__icon" />
+      </div>
     </article>
   )
 }
