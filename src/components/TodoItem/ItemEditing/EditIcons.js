a7e2c9c473d9f7bfe5c37f48fbe366905cdad603
Yura <ynworld85@mac.com>
2023-02-05 21:02:44 +0300

    style: edit and view icons

--- src/components/TodoItem/ItemEditing/EditIcons.js
+++ src/components/TodoItem/ItemEditing/EditIcons.js
@@ -5,11 +5,11 @@ import { CloseIcon, CheckIcon } from '../../icons'
 const ItemEditIcons = ({ handleEditCancel, handleSubmit }) => {
   return (
     <div className="edit__icons">
-      <Button shape="round" className="edit__icon-button" onClick={handleEditCancel}>
-        <CloseIcon className="edit__icon cancel" />
-      </Button>
       <Button shape="round" className="edit__icon-button" onClick={handleSubmit}>
         <CheckIcon className="edit__icon" />
+      </Button>
+      <Button shape="round" className="edit__icon-button" onClick={handleEditCancel}>
+        <CloseIcon className="edit__icon cancel" />
       </Button>
     </div>
   )
