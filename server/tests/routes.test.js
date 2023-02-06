96ed47a42887295482c9c55bb4ec0af5453e2a9a
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-02-06 12:21:00 +0300

    feat: add swagger docs, fix API

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -5,38 +5,44 @@ const _isNumber = require('lodash/isNumber')
 const request = supertest(app)
 
 test('should load todo list', async () => {
-  const res = await request.get('/api/todo-list')
+  const res = await request.get('/todos')
 
   expect(res.statusCode).toBe(200)
-  expect(res.body).toHaveProperty('todoList')
+  expect(res.body).toHaveProperty('items')
 })
 
 test('should create todo item', async () => {
   const testTodoText = 'Test todo'
-  const { body, statusCode } = await request.post('/api/todos').send({ text: testTodoText })
+  const { body, statusCode } = await request.post('/todos').send({ text: testTodoText })
 
   expect(statusCode).toBe(200)
-  expect(body.todoItem.text).toBe(testTodoText)
-  expect(_isNumber(body.todoItem.id)).toBe(true)
+  expect(body.text).toBe(testTodoText)
+  expect(_isNumber(body.id)).toBe(true)
+})
+
+test('should fail to creat todo without text', async () => {
+  const { statusCode } = await request.post('/todos').send({})
+
+  expect(statusCode).toBe(400)
 })
 
 test('should delete todo item', async () => {
-  const { body: { todoItem } } = await request.post('/api/todos').send({ text: 'Test todo' })
+  const { body: { id } } = await request.post('/todos').send({ text: 'Test todo' })
 
-  const { body, statusCode } = await request.delete(`/api/todos/${todoItem.id}`)
+  const { body, statusCode } = await request.delete(`/todos/${id}`)
 
   expect(statusCode).toBe(200)
-  expect(body.todoItem.id).toBe(todoItem.id)
+  expect(body.id).toBe(id)
 })
 
 test('should update todo item', async () => {
-  const { body: { todoItem } } = await request.post('/api/todos').send({ text: 'Test todo' })
-  await request.get('/api/todo-list')
+  const { body: { id } } = await request.post('/todos').send({ text: 'Test todo' })
+  await request.get('/todos')
 
   const editedTodoText = 'Edited todo'
 
-  const { body, statusCode } = await request.put(`/api/todos/${todoItem.id}`).send({ text: editedTodoText })
+  const { body, statusCode } = await request.put(`/todos/${id}`).send({ text: editedTodoText })
 
   expect(statusCode).toBe(200)
-  expect(body.todoItem.text).toBe(editedTodoText)
+  expect(body.text).toBe(editedTodoText)
 })
