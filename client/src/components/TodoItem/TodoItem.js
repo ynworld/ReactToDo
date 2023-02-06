831dc691bf52d94c82b3e79f8f8977523bf26e53
Yura <ynworld85@mac.com>
2023-02-06 18:11:00 +0300

    feat: add edit button

--- client/src/components/TodoItem/TodoItem.js
+++ client/src/components/TodoItem/TodoItem.js
@@ -12,6 +12,9 @@ const TodoItem = ({ todo }) => {
     <article className="todo__list-item">
       <CheckboxField id={id} label={text} isChecked={isChecked} onChange={toggle} />
       <div className="todo__list-icons">
+        <Button className="todo__list-button">
+          <PencilIcon className="todo__list-icon" />
+        </Button>
         <Button className="todo__list-button" onClick={todo.delete}>
           <Icon name={iconNames.trash} className="todo__list-icon" />
         </Button>
