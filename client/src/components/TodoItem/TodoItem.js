e8dca5155a4b3e4274b14536af7df160aae8e49d
Yura <ynworld85@mac.com>
2023-02-06 17:02:15 +0300

    chore: call delete directly from click

--- client/src/components/TodoItem/TodoItem.js
+++ client/src/components/TodoItem/TodoItem.js
@@ -5,15 +5,13 @@ import { CheckboxField, Button } from '../'
 import { TrashIcon } from '../icons'
 
 const TodoItem = ({ todo }) => {
-  const handleItemDelete = () => todo.delete()
-
   const { id, text, isChecked, toggle } = todo
 
   return (
     <article className="todo__list-item">
       <CheckboxField id={id} label={text} isChecked={isChecked} onChange={toggle} />
       <div className="todo__list-icons">
-        <Button className="todo__list-button" onClick={handleItemDelete}>
+        <Button className="todo__list-button" onClick={todo.delete}>
           <TrashIcon className="todo__list-icon" />
         </Button>
       </div>
