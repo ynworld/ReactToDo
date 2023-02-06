a9d406bd167e8952a15f5aa526362fd680e1666c
Yura <ynworld85@mac.com>
2023-02-06 20:09:35 +0300

    feat: generate icons object

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -1,25 +1,21 @@
-import {
-  TrashIcon as TrashIconOutline,
-  PlusCircleIcon as PlusCircleIconOutline,
-} from '@heroicons/react/24/outline'
+import * as IconsOutline from '@heroicons/react/24/outline'
+import * as IconsSolid from '@heroicons/react/24/solid'
 
-import {
-  TrashIcon as TrashIconSolid,
-  PlusCircleIcon as PlusCircleSolid,
-} from '@heroicons/react/24/solid'
+const iconNames = ['PlusCircleIcon', 'TrashIcon']
+
+const icons = {}
+iconNames.forEach((icon) => {
+  const componentOutline = IconsOutline[icon]
+  const componentSolid = IconsSolid[icon]
+  icons[icon] = {
+    outline: componentOutline,
+    solid: componentSolid,
+  }
+})
 
 const Icon = ({ name, className, variant = 'outline' }) => {
-  const icons = {
-    TrashIcon: {
-      outline: <TrashIconOutline className={className} />,
-      solid: <TrashIconSolid className={className} />,
-    },
-    PlusCircleIcon: {
-      outline: <PlusCircleIconOutline className={className} />,
-      solid: <PlusCircleSolid className={className} />,
-    },
-  }
-  return icons[name][variant]
+  const RenderIcon = icons[name][variant]
+  return <RenderIcon className={className} />
 }
 
 export default Icon
