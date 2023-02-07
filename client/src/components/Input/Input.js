ae578c23f41db2a1fc27ceda2b1cb0acb6bfab60
Yura <ynworld85@mac.com>
2023-02-07 22:16:10 +0300

    feat: add Input component

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,14 @@
+const Input = ({ value, onChange }) => {
+  return (
+    <input
+      type="text"
+      className="todo__list-input"
+      placeholder="I need to..."
+      autoFocus
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
+
+export default Input
