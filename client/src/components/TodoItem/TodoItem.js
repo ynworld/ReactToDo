0c3f42f40aba397ac5047553e989daf2ecbd0f42
Yura <ynworld85@mac.com>
2023-02-07 11:45:45 +0300

    style: input field, chore: temp TodoItem isEditing toggle

--- client/src/components/TodoItem/TodoItem.js
+++ client/src/components/TodoItem/TodoItem.js
@@ -6,18 +6,27 @@ import { iconNames } from '../../constants'
 
 const TodoItem = ({ todo }) => {
   const { id, text, isChecked, toggle } = todo
+  const isEditing = false
 
   return (
     <article className="todo__list-item">
-      <CheckboxField id={id} label={text} isChecked={isChecked} onChange={toggle} />
-      <div className="todo__list-icons">
-        <Button className="todo__list-button">
-          <Icon name={iconNames.pencil} className="todo__list-icon" />
-        </Button>
-        <Button className="todo__list-button" onClick={todo.delete}>
-          <Icon name={iconNames.trash} className="todo__list-icon todo__list-icon--red" />
-        </Button>
-      </div>
+      {isEditing ? (
+        <>
+          <ItemEdit todo={todo} />
+        </>
+      ) : (
+        <>
+          <CheckboxField id={id} label={text} isChecked={isChecked} onChange={toggle} />
+          <div className="todo__list-icons">
+            <Button className="todo__list-button">
+              <Icon name={iconNames.pencil} className="todo__list-icon" />
+            </Button>
+            <Button className="todo__list-button" onClick={todo.delete}>
+              <Icon name={iconNames.trash} className="todo__list-icon todo__list-icon--red" />
+            </Button>
+          </div>
+        </>
+      )}
     </article>
   )
 }
