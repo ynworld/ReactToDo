f2b02d8052c077cd0c59af50f4e108df40378cc1
Yura <ynworld85@mac.com>
2023-02-09 12:45:01 +0300

    chore: replace App.css with Tailwind classes

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import AppStore from './stores/AppStore'
 
 import { Button, CompletionBar, TodoList, Loading, Icon } from './components'
 
-import './App.css'
-
 const App = () => {
   const [appStore] = useState(() => new AppStore())
 
@@ -18,23 +16,22 @@ const App = () => {
   const { isLoading, todoList } = appStore
 
   return (
-    <main className="wrapper">
+    <main className="relative m-auto p-6 max-w-2xl">
       <CompletionBar percentComplete={todoList.percentComplete} />
 
       {isLoading ? (
         <Loading text="Tasks are Loading" />
       ) : (
         <>
-          <h1 className="title">Tasks</h1>
+          <h1 className="title text-8xl text-gray-800 font-extrabold">Tasks</h1>
           <TodoList todos={todoList.items} />
           <Button
-            shape="round"
             position="top-right"
-            className="add-button"
+            className="w-20 h-20 rounded-full bg-primary shadow-lg shadow-gray-400 hover:bg-primary-dark focus:bg-primary-dark active:shadow-md disabled:bg-gray-400 disabled:shadow-lg disabled:shadow-gray-400 disabled:cursor-not-allowed  transition-all duration-300"
             disabled={todoList.hasItemInEditingMode}
             onClick={todoList.addItem}
           >
-            <Icon name={iconNames.plusCircle} className="add-icon" />
+            <Icon name={iconNames.plusCircle} className="w-3/4 text-gray-100" />
           </Button>
         </>
       )}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./src/**/*.js'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: colors.zinc,
+      green: colors.lime,
+      primary: {
+        light: '#56de45',
+        DEFAULT: '#4fc41d',
+        dark: '#4cb51d',
+      },
+    },
     extend: {},
   },
   plugins: [],
