5a328d884edbdedc2fa22f02b5e68389faffee68
Yura <ynworld85@mac.com>
2023-02-19 22:58:16 +0300

    chore: add DOM element for Floating Portal

--- client/src/components/Popover/PopoverContent.js
+++ client/src/components/Popover/PopoverContent.js
@@ -1,7 +1,7 @@
 import { PropTypes } from 'prop-types'
 import { forwardRef } from 'react'
 
-import { useMergeRefs, FloatingFocusManager } from '@floating-ui/react'
+import { useMergeRefs, FloatingFocusManager, FloatingPortal } from '@floating-ui/react'
 import { usePopoverContext } from './Popover'
 
 export const PopoverContent = forwardRef((props, propRef) => {
@@ -12,22 +12,24 @@ export const PopoverContent = forwardRef((props, propRef) => {
 
   return (
     context.open && (
-      <FloatingFocusManager context={floatingContext} initialFocus={-1} modal={context.modal}>
-        <div
-          ref={ref}
-          style={{
-            left: context.x ?? 0,
-            position: context.strategy,
-            top: context.y ?? 0,
-            width: 'max-content',
-            ...props.style,
-            ...styles,
-          }}
-          {...context.getFloatingProps(props)}
-        >
-          {props.children}
-        </div>
-      </FloatingFocusManager>
+      <FloatingPortal id="float">
+        <FloatingFocusManager context={floatingContext} initialFocus={-1} modal={context.modal}>
+          <div
+            ref={ref}
+            style={{
+              left: context.x ?? 0,
+              position: context.strategy,
+              top: context.y ?? 0,
+              width: 'max-content',
+              ...props.style,
+              ...styles,
+            }}
+            {...context.getFloatingProps(props)}
+          >
+            {props.children}
+          </div>
+        </FloatingFocusManager>
+      </FloatingPortal>
     )
   )
 })
