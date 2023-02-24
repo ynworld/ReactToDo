0c0d38dc9b4eec207712de3157c1669b831a130f
Yura <ynworld85@mac.com>
2023-02-24 23:03:04 +0300

    fix: completion bar always on top

--- client/src/components/CompletionBar/CompletionBar.js
+++ client/src/components/CompletionBar/CompletionBar.js
@@ -2,7 +2,7 @@ import { PropTypes } from 'prop-types'
 
 const CompletionBar = ({ percentComplete }) => (
   <div
-    className="fixed bottom-0 left-0 h-1 bg-primary transition-all ease-in-out"
+    className="fixed bottom-0 left-0 z-10 h-1 bg-primary transition-all ease-in-out"
     style={{ width: `${percentComplete}%` }}
   />
 )
