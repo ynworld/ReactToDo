cee26fcfc1afb57db8824f18b1a89d66e5ff9904
Yura <ynworld85@mac.com>
2023-02-28 13:15:37 +0300

    chore: refactor Tooltip

--- client/src/components/Tooltip/Tooltip.js
+++ client/src/components/Tooltip/Tooltip.js
@@ -1,20 +1,19 @@
 import { PropTypes } from 'prop-types'
 import { TooltipProvider, TooltipTrigger, TooltipContent } from '.'
 
-const Tooltip = ({ children, content, className, ...options }) => {
-  return (
+const Tooltip = ({ children, content, className, ...options }) =>
+  content ? (
     <TooltipProvider options={options}>
       <TooltipTrigger className={className}>{children}</TooltipTrigger>
       <TooltipContent>
-        {content ? (
-          <div className="rounded-md bg-black py-1 px-2 text-xs text-white shadow-md sm:max-w-sm">
-            {content}
-          </div>
-        ) : null}
+        <div className="rounded-md bg-black py-1 px-2 text-xs text-white shadow-md sm:max-w-sm">
+          {content}
+        </div>
       </TooltipContent>
     </TooltipProvider>
+  ) : (
+    children
   )
-}
 
 Tooltip.propTypes = {
   children: PropTypes.node,
