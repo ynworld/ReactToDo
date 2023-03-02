8abe5c06a5d7f6abd6c1c644db33924b182eabcd
Yura <ynworld85@mac.com>
2023-03-02 23:57:20 +0300

    chore: extract AddItemForm Component

--- client/src/components/AddItemModal/AddItemModal.js
+++ client/src/components/AddItemModal/AddItemModal.js
@@ -2,25 +2,12 @@ import { PropTypes } from 'prop-types'
 import { useState } from 'react'
 import classnames from 'classnames'
 import { Modal, ModalContent, Icon } from '..'
+import AddItemForm from './AddItemForm'
 import { TodoListStore } from '../../stores/TodoListStore'
 import { iconNames } from '../../constants'
-import { post } from '../../api'
 
 const AddItemModal = ({ todoList }) => {
   const [isOpen, setIsOpen] = useState(false)
-  const [inputText, setInputText] = useState('')
-
-  const handleTextInput = (event) => {
-    setInputText(event.target.value)
-  }
-
-  const handleAddItem = (event) => {
-    event.preventDefault()
-    post('/todos', { text: inputText }).then((todoItem) => {
-      todoList.addItem(todoItem)
-      setIsOpen(false)
-    })
-  }
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -51,38 +38,7 @@ const AddItemModal = ({ todoList }) => {
             <Icon name={iconNames.xmark} />
           </button>
         </div>
-        <form className="flex w-full flex-col gap-4" onSubmit={handleAddItem}>
-          <input
-            className={classnames(
-              'h-8 grow rounded-md border-2 border-primary px-2 text-sm',
-              'outline-none transition-all duration-300 focus:shadow-md focus:shadow-primary/25',
-            )}
-            onChange={handleTextInput}
-            placeholder="I need to..."
-            type="text"
-          />
-          <div className="flex justify-end gap-2">
-            <button
-              className={classnames(
-                'flex h-8 items-center rounded-md px-6 py-2 text-sm shadow-md',
-                'hover:bg-gray-100 active:shadow-sm',
-              )}
-              onClick={() => setIsOpen(false)}
-              type="button"
-            >
-              Cancel
-            </button>
-            <button
-              className={classnames(
-                'flex h-8 items-center rounded-md bg-primary px-6 py-2 text-sm text-white shadow-md',
-                'hover:bg-primary-dark active:shadow-sm',
-              )}
-              type="submit"
-            >
-              Add
-            </button>
-          </div>
-        </form>
+        <AddItemForm setIsOpen={setIsOpen} todoList={todoList} />
       </ModalContent>
     </Modal>
   )
