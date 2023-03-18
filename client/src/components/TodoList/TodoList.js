94041d4b6e67f7ba33d639cf9e393532f262e134
Yuriy Nayer <109351999+ynworld@users.noreply.github.com>
2023-03-18 19:32:37 +0300

    Add React Transition Group (#52)

--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -1,33 +1,62 @@
 import { PropTypes } from 'prop-types'
 import { observer } from 'mobx-react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { TodoListStore } from '../../stores/TodoListStore'
 
 import { DragPreview } from '../DragPreview'
 import { TodoItem, TodoItemWithDnd } from '../TodoItem'
 
+const transitionClassNames = {
+  appear: 'opacity-0',
+  appearActive: 'opacity-100 transition-opacity duration-500',
+  enter: 'opacity-0',
+  enterActive: 'opacity-100 transition-opacity duration-500',
+  exitActive: 'opacity-0 transition-opacity duration-500',
+}
+
+const transitionTimeout = 500
+
 const TodoList = ({ todoListStore }) => {
   const { newItem, importantItems, regularItems } = todoListStore
 
   return (
-    <ul className="mx-0 my-8 flex flex-col gap-4">
+    <TransitionGroup className="mx-0 my-8 flex flex-col gap-4" component="ul">
       {newItem && (
         <li>
           <TodoItem todo={newItem} />
         </li>
       )}
-      {importantItems.map((todo) => (
-        <li key={todo.id}>
-          <TodoItem todo={todo} />
-        </li>
-      ))}
-      {regularItems.map((todo, index) => (
-        <li key={todo.id}>
-          <TodoItemWithDnd index={index} todo={todo} />
-        </li>
-      ))}
+      {importantItems.map((todo) => {
+        return (
+          <CSSTransition
+            key={todo.id}
+            appear
+            classNames={transitionClassNames}
+            timeout={transitionTimeout}
+          >
+            <li>
+              <TodoItem todo={todo} />
+            </li>
+          </CSSTransition>
+        )
+      })}
+      {regularItems.map((todo, index) => {
+        return (
+          <CSSTransition
+            key={todo.id}
+            appear
+            classNames={transitionClassNames}
+            timeout={transitionTimeout}
+          >
+            <li>
+              <TodoItemWithDnd index={index} todo={todo} />
+            </li>
+          </CSSTransition>
+        )
+      })}
       <DragPreview />
-    </ul>
+    </TransitionGroup>
   )
 }
 
