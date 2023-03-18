51c8756dabad7acbf8a83f26788b1f728c998ed0
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-03-18 19:28:28 +0300

    chore: apply transition group

--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -1,83 +1,62 @@
 import { PropTypes } from 'prop-types'
 import { observer } from 'mobx-react'
-import { useRef, useEffect, useState } from 'react'
-import { Transition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { TodoListStore } from '../../stores/TodoListStore'
 
 import { DragPreview } from '../DragPreview'
 import { TodoItem, TodoItemWithDnd } from '../TodoItem'
 
+const transitionClassNames = {
+  appear: 'opacity-0',
+  appearActive: 'opacity-100 transition-opacity duration-500',
+  enter: 'opacity-0',
+  enterActive: 'opacity-100 transition-opacity duration-500',
+  exitActive: 'opacity-0 transition-opacity duration-500',
+}
+
+const transitionTimeout = 500
+
 const TodoList = ({ todoListStore }) => {
   const { newItem, importantItems, regularItems } = todoListStore
 
-  const [appear, setAppear] = useState(false)
-
-  useEffect(() => {
-    setAppear(true)
-  }, [])
-
-  const transitionStyles = {
-    entered: { opacity: 1 },
-    entering: { opacity: 1 },
-    exited: { opacity: 0 },
-    exiting: { opacity: 0 },
-  }
-
-  const nodeRef = useRef(null)
-
   return (
-    <ul className="mx-0 my-8 flex flex-col gap-4">
+    <TransitionGroup className="mx-0 my-8 flex flex-col gap-4" component="ul">
       {newItem && (
         <li>
           <TodoItem todo={newItem} />
         </li>
       )}
-      {importantItems.map((todo, index) => {
-        const duration = 400 + index * 200
-
+      {importantItems.map((todo) => {
         return (
-          <Transition key={todo.id} in={appear} nodeRef={nodeRef} timeout={duration}>
-            {(state) => (
-              <li
-                key={todo.id}
-                ref={nodeRef}
-                style={{
-                  opacity: 0,
-                  transition: `opacity ${duration}ms ease-in-out`,
-                  ...transitionStyles[state],
-                }}
-              >
-                <TodoItem todo={todo} />
-              </li>
-            )}
-          </Transition>
+          <CSSTransition
+            key={todo.id}
+            appear
+            classNames={transitionClassNames}
+            timeout={transitionTimeout}
+          >
+            <li key={todo.id}>
+              <TodoItem todo={todo} />
+            </li>
+          </CSSTransition>
         )
       })}
       {regularItems.map((todo, index) => {
-        const importantCount = todoListStore.importantItems.length
-        const duration = 400 + (index + importantCount - 1) * 200
-
         return (
-          <Transition key={todo.id} in={appear} nodeRef={nodeRef} timeout={duration}>
-            {(state) => (
-              <li
-                key={todo.id}
-                ref={nodeRef}
-                style={{
-                  opacity: 0,
-                  transition: `opacity ${duration}ms ease-in-out`,
-                  ...transitionStyles[state],
-                }}
-              >
-                <TodoItemWithDnd index={index} todo={todo} />
-              </li>
-            )}
-          </Transition>
+          <CSSTransition
+            key={todo.id}
+            appear
+            classNames={transitionClassNames}
+            timeout={transitionTimeout}
+          >
+            <li key={todo.id}>
+              <TodoItemWithDnd index={index} todo={todo} />
+            </li>
+          </CSSTransition>
         )
       })}
       <DragPreview />
-    </ul>
+    </TransitionGroup>
   )
 }
 
