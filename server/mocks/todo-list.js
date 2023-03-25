96b77396c6d830351d05fd73a5c2c4559b2ece18
Aleksandr Dus <aleksandr.dus@scompler.com>
2023-03-25 13:40:15 +0300

    chore: change date type for saved entries

--- server/mocks/todo-list.js
+++ server/mocks/todo-list.js
@@ -1,53 +1,60 @@
+const createdAt = Date.now()
+
 const todoItems = [
   {
-    text: 'Dentist Appointment - Prepare the files and do the necessary tests suggested beforehand.',
-    isChecked: false,
-    isImportant: false,
     createdAt: '2022-12-11T00:00:00.000Z',
     id: 1,
-  },
-  {
-    text: 'CS-121 Assignment Deadline on Monday',
     isChecked: false,
     isImportant: false,
+    text: 'Dentist Appointment - Prepare the files and do the necessary tests suggested beforehand.',
+  },
+  {
     createdAt: '2022-12-15T00:00:00.000Z',
     id: 2,
-  },
-  {
-    text: 'Get the electric bulb repaired',
     isChecked: false,
     isImportant: false,
+    text: 'CS-121 Assignment Deadline on Monday',
+  },
+  {
     createdAt: '2022-12-17T00:00:00.000Z',
     id: 3,
+    isChecked: false,
+    isImportant: false,
+    text: 'Get the electric bulb repaired',
   },
   {
-    text: 'Soccer Club Meeting @ Sunday',
-    isChecked: true,
-    isImportant: false,
     createdAt: '2022-12-18T00:00:00.000Z',
     id: 4,
-  },
-  {
-    text: "Buy Gift for Dad's Birthday",
     isChecked: true,
     isImportant: false,
+    text: 'Soccer Club Meeting @ Sunday',
+  },
+  {
     createdAt: '2022-12-19T00:00:00.000Z',
     id: 5,
-  },
-  {
-    text: 'Submit Assignment on Friday',
     isChecked: true,
     isImportant: false,
+    text: "Buy Gift for Dad's Birthday",
+  },
+  {
     createdAt: '2022-12-20T00:00:00.000Z',
     id: 6,
-  },
-  {
-    text: 'Bring Groceries From Supermarket',
     isChecked: true,
     isImportant: false,
+    text: 'Submit Assignment on Friday',
+  },
+  {
     createdAt: '2022-12-21T00:00:00.000Z',
     id: 7,
+    isChecked: true,
+    isImportant: false,
+    text: 'Bring Groceries From Supermarket',
   },
 ]
 
-module.exports = todoItems
+const dateModifiedItems = todoItems.map((item, index) => ({
+  ...item,
+  createdAt: createdAt + index * 1000,
+}))
+
+module.exports = dateModifiedItems
